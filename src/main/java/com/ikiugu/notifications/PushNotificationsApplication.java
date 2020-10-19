792f2d4c8681bfe35bd18445518ef487e26d8d6b
Alfred Ikiugu <ikiugualf@gmail.com>
2020-10-19 23:00:53 +0300

    initial commit

---
+++ src/main/java/com/ikiugu/notifications/PushNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.ikiugu.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikiugu/notifications/PushNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikiugu.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
